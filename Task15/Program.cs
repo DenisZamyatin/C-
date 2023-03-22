997f5e231d88566c179c0dbe27be5859be940c25
«Denis <zlockoznenniylokki@gmail.com>
2023-03-22 22:32:16 +0700

    Решили задачу 16

---
+++ Task15/Program.cs
@@ -0,0 +1,24 @@
+﻿ Console.Write ("Введите число от 1 до 7: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 = number1<=5)
+
+{
+    Console.Write ("Нет");
+}
+
+if (number1= number1<=7)
+
+{
+    Console.Write ("Выходной");
+}
+
+else (number1 = number1>7);
+
+{
+    Console.Write ("Не верное число");
+}
+    // if (result == 0)
+    //     Console.WriteLine("Кратно");
+    // else
+    //     Console.WriteLine($"Не кратно, остаток {result}");
